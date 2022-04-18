cde3ba078f8ee6aad12c86c7b8f26ede2cee1a76
Jamal Adrien <jaa249@pitt.edu>
2022-04-18 18:00:48 -0400

    commit5

--- R/UnadjustedMR.R
+++ R/UnadjustedMR.R
@@ -2,17 +2,17 @@
 
 CRUDE <- function(Population, Deaths, Rate){
 
-  if (Deaths > PopulationAtRisk){
+  if (Deaths > Population){
     rlang::catch_cnd(abort(message = "Deaths are greater than Population At Risk! Check for errors in the data source!",
                            .subclass = "death_overflow"
     ))
 
   } else {
     if (missing(Rate) || Rate ==  FALSE){
-      (Deaths / (PopulationAtRisk)) * 100
+      (Deaths / (Population)) * 100
      } else {
        if (Rate == TRUE){
-       (Deaths / (PopulationAtRisk))
+       (Deaths / (Population))
        }
       }
     }
